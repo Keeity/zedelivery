3e913166211c51143ca96aa5b26f233059e20314
Keeity Braga Collodel <keeity@hotmail.com>
2024-07-21 11:57:21 -0300

    template ajustado

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Sidebar from "./components/Sidebar/Sidebar"
 import Header from "./components/Header/Header"
 import './App.css'
 import RoutesComponent from './routes/Routes'
+import Footer from './components/Footer/Footer'
 
+Footer
 function App() {
   // const { isAuthenticated } = useAuth()
   const isAuthenticated = true;
@@ -16,10 +18,11 @@ function App() {
         {isAuthenticated ? 
         (
           <div className='grid-container'>
-            <Header />
+            {/* A sintaxe <Header /> é equivalente a chamar React.createElement(Header). Nao tem abertura porque nao precisa escrever nada dentro e nao tem*/}
+            <Header />  
             <Sidebar />
-
             <RoutesComponent />
+            <Footer />
           </div>
         ) 
         : (
--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react'
+import './FormField.css'
+import { useNavigate } from 'react-router-dom'
 
-function FormFieldComponent() {
+
+function FormField() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const navigate = useNavigate()
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        try {
+           if (email === 'admin@admin.com' && password === 'admin123') {
+              // redireciona para algum lugar
+                  navigate('/home')
+            } else {
+                alert('Usuário ou senha incorretos')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
-        <>
-            <input type="text"/>
-            <button>Buscar</button>
-        </>
+        <form onSubmit={handleSubmit}>
+        <input placeholder="Email" type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button type="submit">Login</button>
+    </form>
     )
 }
 
-export default FormFieldComponent
+export default FormField
